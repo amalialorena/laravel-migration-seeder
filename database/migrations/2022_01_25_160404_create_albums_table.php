3e6da6c2e7fab97f565cd09c2c08a0ab40fbb6c3
Amalia <amalialorenaferariu@gmail.com>
2022-01-25 17:41:29 +0100

    music shop db

---
+++ database/migrations/2022_01_25_160404_create_albums_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('songs');
+            $table->date('release_date');
+            $table->string('cover')->nullable;
+            $table->bigInteger('artist_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
